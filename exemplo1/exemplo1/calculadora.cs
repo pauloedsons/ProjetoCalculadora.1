6ee83bdf3a369fe86485e6917139f42eaba8b081
Paulo Edson <paulo_edsons@hotmail.com>
2016-07-10 02:33:14 -0300

    Criei um projeto de exemplo

---
+++ exemplo1/exemplo1/calculadora.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplo1
+{
+    class Calculadora
+    {
+        static void Main(string[] args)
+                    {
+                    }
+
+        public int Soma (int a, int b)
+        {
+            return a + b;
+        }
+
+
+    }
+}
